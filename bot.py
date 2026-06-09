b11f74f48877074c5114cc3c25aa30ce2b4c582f
vproptradercom-dot <vproptrader.com@gmail.com>
2026-06-09 20:10:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ def keep_alive():
 # --- End Flask Keep Alive ---
 
 # --- Configuration ---
-TOKEN = '8240685892:AAHAqX5hozFSMsqmIslxRrKWclbP4Wt8W3U' # Replace with your actual token
+TOKEN = '8750705036:AAFviZUFZgy_vjUMo2ydrfOHy2k19P-fnlo' # Replace with your actual token
 OWNER_ID = 7891441948 # Replace with your Owner ID
 ADMIN_ID = 7987480973 # Replace with your Admin ID (can be same as Owner)
 YOUR_USERNAME = 'MonsterPapaa' # Replace with your Telegram username (without the @)
